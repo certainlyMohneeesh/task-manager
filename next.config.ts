766159d6ac650bdb217a44135e245f39a148bf9c
CertainlyMohneesh <133652369+certainlyMohneeesh@users.noreply.github.com>
2024-11-23 01:26:31 +0530

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/<your-repo-name>',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true
   },
   eslint : {
     ignoreDuringBuilds: true
   }
+
 };
 
 export default nextConfig;
